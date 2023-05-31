5d3cbc965b3a10d1bbc675720221cc470dc87380
Nishant Patel <60250103+np5499@users.noreply.github.com>
2023-05-31 17:27:51 -0400

    initial files

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const navbar = (
+    <nav>
+        <h1>React page</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+ReactDOM.render(navbar , document.getElementById("root"))
