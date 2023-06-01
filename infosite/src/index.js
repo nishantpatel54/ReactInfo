b88c30bda944a3585adcaa6ba58f24001f4b2ce2
Nishant Patel <60250103+np5499@users.noreply.github.com>
2023-06-01 02:18:04 -0400

    challenge part 2 for components

--- infosite/src/index.js
+++ infosite/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import logo from './logo.svg';
 
 // const page = (
@@ -18,31 +17,35 @@ import logo from './logo.svg';
 //     </div>
 // )
 
-function Factslist(){
-  return (
-    <ol>
-      <li>Was first released in 2013</li>
-      <li>Was Originally created by Jordan Walke</li>
-      <li>Has well over 100k stars on GitHub</li>
-      <li>Its maintained by facebook</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ol>
-  );
-}
+// function Factslist(){
+//   return (
+//     <ol>
+//       <li>Was first released in 2013</li>
+//       <li>Was Originally created by Jordan Walke</li>
+//       <li>Has well over 100k stars on GitHub</li>
+//       <li>Its maintained by facebook</li>
+//       <li>Powers thousands of enterprise apps, including mobile apps</li>
+//     </ol>
+//   );
+// }
 
-function Componented(){
-  return (
-    <div>
-      <img src={logo} className="App-logo" alt="logo" width="10%"/>
-      <h1>Fun facts about react</h1>
-      <Factslist/>
-    </div>
-  )
-}
+// function Componented(){
+//   return (
+//     <div>
+//       <h1>Fun facts about react</h1>
+//       <Factslist/>
+//     </div>
+//   )
+// }
 
 function WhyLoveReact(){
   return(
     <div>
+      <header>
+        <nav>
+          <img src={logo} className="App-logo" alt="logo" width="10%"/>
+        </nav>
+      </header>
       <h1>Why I love react</h1>
       <ol>
         <li>I need a job bruh</li>
@@ -50,10 +53,15 @@ function WhyLoveReact(){
         <li>much faster and better than angular</li>
         <li>need it for my portfolio website</li>
       </ol>
+      <footer>
+        <p> &copy; 2023 Patel development. All rights reserved</p>
+      </footer>
     </div>
   );
 }
 
-ReactDOM.render(<Componented/>, document.getElementById("root"));
-
 ReactDOM.render(<WhyLoveReact/>, document.getElementById("list"));
+
+// ReactDOM.render(<Componented/>, document.getElementById("root"));
+
+
