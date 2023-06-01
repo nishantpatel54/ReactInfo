eca2161937dba0cfa51c0b962c3673cae28de2c9
Nishant Patel <60250103+np5499@users.noreply.github.com>
2023-06-01 03:03:16 -0400

    added styling to page

--- infosite/src/index.js
+++ infosite/src/index.js
@@ -44,9 +44,9 @@ function Header(){
       <nav className='nav'>
         <img src={logo} className="App-logo" alt="logo"/>
         <ul className='nav-items'>
-          <li>Pricing</li>
-          <li>About</li>
-          <li>Contact</li>
+          <li><a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley'>Pricing</a></li>
+          <li><a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley'>About</a></li>
+          <li><a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley'>Contact</a></li>
         </ul>
       </nav>
     </header>
