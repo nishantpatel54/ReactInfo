ccc1339ae27fe8937496119e1fe32c8e41c1457b
Nishant Patel <60250103+np5499@users.noreply.github.com>
2023-06-01 01:03:47 -0400

    added my app to info page

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-import React from "react";
+// import React from "react";
+// import ReactDOM from "react-dom"
 
 const navbar = (
     <nav>
         <h1>React page</h1>
-        <ul>
+        <ol>
             <li>Pricing</li>
             <li>About</li>
             <li>Contact</li>
-        </ul>
+        </ol>
+        <p>Hello world</p>
     </nav>
 )
+
 ReactDOM.render(navbar , document.getElementById("root"))
